879f8fe1baf4dfbde279f07773c4196384bd2723
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-09-30 12:40:39 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/Header/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Meh Clothing</h1>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
